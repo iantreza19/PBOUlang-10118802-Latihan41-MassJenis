b132ca00a47ba68d3bafb93927ef2ec2cb4d3133
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-11-07 22:35:07 +0700

    Add files via upload

---
+++ PBOUlang10118802Latihan41/src/Kubus.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class Kubus {
+    private int sisi, massa;
+
+    public int getSisi() {
+        return sisi;
+    }
+
+    public void setSisi(int sisi) {
+        this.sisi = sisi;
+    }
+
+    public int getMassa() {
+        return massa;
+    }
+
+    public void setMassa(int massa) {
+        this.massa = massa;
+    }
+
+    public int hitungVolume(int parSisi) {
+        return sisi * sisi * sisi;
+
+    }
+
+    public int hitungMassaJenis(int parMassa, int volume) {
+        return massa / (sisi * sisi * sisi);
+
+    }
+}
